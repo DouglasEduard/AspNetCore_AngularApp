8b776dee6641c8f53a16c727e1f965a62f768040
Douglas Eduardo Silvério <douglassilverio.it@gmail.com>
2019-04-03 23:14:05 +0100

    First commit

---
+++ EasyDevelopersApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using EasyDevelopersApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EasyDevelopersApp.API.Data 
+{
+    public class DataContext: DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }        
+    }
+}
