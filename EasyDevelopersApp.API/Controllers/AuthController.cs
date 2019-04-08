0816e6617de2dc82bd8615d860180df2e0efc97c
Douglas Eduardo Silvério <douglassilverio.it@gmail.com>
2019-04-09 00:06:20 +0100

    AuthController

---
+++ EasyDevelopersApp.API/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+using EasyDevelopersApp.API.Data;
+using EasyDevelopersApp.API.Dtos;
+using EasyDevelopersApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyDevelopersApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]    
+    public class AuthController: ControllerBase
+    {
+       public IAuthRepository _repo { get; }
+
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }   
+
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            //validate request
+
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if (await _repo.UserExistis(userForRegisterDto.Username))
+                return BadRequest("User name already exists");
+
+            var UserToCreate = new User
+            {
+                UserName = userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(UserToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);            
+        }
+    }
+}
