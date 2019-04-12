90f1ec9fa7988b7a8ac704c2c1e96b0a62ca85fd
Douglas Eduardo Silvério <douglassilverio.it@gmail.com>
2019-04-13 00:57:56 +0100

    Token Authentication

--- EasyDevelopersApp.API/Controllers/AuthController.cs
+++ EasyDevelopersApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using EasyDevelopersApp.API.Data;
 using EasyDevelopersApp.API.Dtos;
 using EasyDevelopersApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace EasyDevelopersApp.API.Controllers
 {
@@ -11,10 +17,12 @@ namespace EasyDevelopersApp.API.Controllers
     public class AuthController: ControllerBase
     {
        public IAuthRepository _repo { get; }
+        public IConfiguration _config { get; }
 
-        public AuthController(IAuthRepository repo)
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
             _repo = repo;
+            _config = config;
         }   
         
         [HttpPost("register")]
@@ -35,6 +43,41 @@ namespace EasyDevelopersApp.API.Controllers
             var createdUser = await _repo.Register(UserToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);            
+        }
+
+        [HttpPost("login")]
+
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddMinutes(20),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new { 
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
