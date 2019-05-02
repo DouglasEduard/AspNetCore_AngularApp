f699508fff6894dd3dbc7c7b1028a82a84377507
Douglas Eduardo Silvério <douglassilverio.it@gmail.com>
2019-05-02 23:55:04 +0100

    Error Handling

--- EasyDevelopersApp.API/Startup.cs
+++ EasyDevelopersApp.API/Startup.cs
@@ -15,6 +15,10 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using EasyDevelopersApp.API.Helpers;
 
 namespace EasyDevelopersApp.API
 {
@@ -54,7 +58,19 @@ namespace EasyDevelopersApp.API
             }
             else
             {
-                //app.UseHsts();
+                app.UseExceptionHandler(builder => {
+                    builder.Run(async context => {
+                       context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+
+                        if (error != null) 
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
             }
 
             //app.UseHttpsRedirection();
